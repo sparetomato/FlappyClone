a4def25da51f8f8b810a45322f72c0267c0c6144
Andy <sparetomato@gmail.com>
2024-01-31 22:05:47 +0000

    Randomise the position of the pipe and delete after it reaches a certain x position

--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -7,17 +7,23 @@ public class PipeMoveScript : MonoBehaviour
 
 
     public float moveSpeed = 5;
+    public float deleteAfter = -30;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        if (transform.position.x < deleteAfter)
+        {
+            Debug.Log("Pipe Deleted");
+            Destroy(gameObject);
+        }
         
     }
 }
--- Assets/PipeSpawnScript.cs
+++ Assets/PipeSpawnScript.cs
@@ -6,13 +6,14 @@ public class PipeSpawnScript : MonoBehaviour
 {
 
     public GameObject pipe;
-    public float spawnRate = 2;
+    public float spawnRate = 1.5F;
     private float timer = 0;
+    public float heightOffset = 10;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnPipe();
     }
 
     // Update is called once per frame
@@ -24,8 +25,16 @@ public class PipeSpawnScript : MonoBehaviour
         }
         else
         {
-            Instantiate(pipe, transform.position, transform.rotation);
+            //spawnPipe();
             timer = 0;
         }
+    }
+
+    void spawnPipe()
+    {
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+
+        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
     }
 }
